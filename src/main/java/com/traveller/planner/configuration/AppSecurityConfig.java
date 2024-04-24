6d8f7874d5feb820b98a01ab6c34995ec185f9c5
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-04-24 16:25:38 +0200

    init security config

---
+++ src/main/java/com/traveller/planner/configuration/AppSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.traveller.planner.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig {
+
+}
---
+++ src/main/java/com/traveller/planner/controller/ContentController.java
@@ -0,0 +1,8 @@
+package com.traveller.planner.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ContentController {
+
+}
