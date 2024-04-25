ca1dfd14126c622d6350ca5adc3dc92f9c317381
AhmedBakrXI <ahmedbakr2501@gmail.com>
2024-04-25 21:31:46 +0200

    update

--- src/main/java/com/traveller/planner/configuration/AppSecurityConfig.java
+++ src/main/java/com/traveller/planner/configuration/AppSecurityConfig.java
@@ -4,18 +4,14 @@ import com.traveller.planner.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,7 +27,8 @@ public class AppSecurityConfig {
                 .authorizeHttpRequests(registry -> {
                     registry.requestMatchers("/css/**", "/js/**", "/images/**").permitAll();
                     registry.requestMatchers("/login", "/register").permitAll();
-                    registry.anyRequest().authenticated();
+//                    registry.anyRequest().authenticated();
+                    registry.anyRequest().permitAll();
                 })
                 .formLogin(httpSecurityFormLoginConfigurer -> {
                     httpSecurityFormLoginConfigurer
