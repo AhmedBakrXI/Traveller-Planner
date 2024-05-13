00766ea50fc5238c109674b680207cdc1af1973f
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2024-05-14 00:34:35 +0300

    Update CityService.java

--- src/main/java/com/traveller/planner/service/CityService.java
+++ src/main/java/com/traveller/planner/service/CityService.java
@@ -1,3 +1,6 @@
+/**
+ * This service class provides methods to interact with city data.
+ */
 package com.traveller.planner.service;
 
 import com.traveller.planner.model.CityModel;
@@ -12,20 +15,35 @@ import java.util.List;
 
 @Service
 public class CityService {
+
     @Autowired
     private CityRepository cityRepository;
 
     @Autowired
     private UserService userService;
 
+    /**
+     * Retrieves the city of the currently authenticated user.
+     * @return A CityModel object representing the city of the currently authenticated user.
+     */
     public CityModel getUserCity() {
+        // Retrieve currently logged-in user's username
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
+        
+        // Retrieve user model based on the username
         UserModel userModel = userService.getUser(username);
+        
+        // Retrieve the city model by the user's city name
         return cityRepository.findByCityName(userModel.getCity());
     }
 
+    /**
+     * Retrieves all cities.
+     * @return A list of CityModel objects representing all cities in the database.
+     */
     public List<CityModel> getAllCities() {
+        // Retrieve all cities from the repository
         return cityRepository.findAll();
     }
 }
