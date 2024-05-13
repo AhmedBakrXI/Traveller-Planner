ba592caec5d4aa5592d6e4692d3ff298c1a7b437
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2024-05-14 00:19:09 +0300

    Update UserService.java

--- src/main/java/com/traveller/planner/service/UserService.java
+++ src/main/java/com/traveller/planner/service/UserService.java
@@ -1,3 +1,7 @@
+/**
+ * The UserService class provides methods for user registration, retrieval, and authentication.
+ * It interacts with the UserRepository to perform CRUD operations on User entities.
+ */
 package com.traveller.planner.service;
 
 import com.traveller.planner.model.UserModel;
@@ -8,22 +12,41 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-
+    /**
+     * Registers a new user by encoding the password using the password encoder
+     * and saving the user details to the database.
+     *
+     * @param user The UserModel object representing the user to be registered.
+     */
     public void registerUser(UserModel user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
     }
 
+    /**
+     * Retrieves a user by their username.
+     *
+     * @param userName The username of the user to be retrieved.
+     * @return The UserModel object representing the user, or null if not found.
+     */
     public UserModel getUser(String userName) {
         return userRepository.findByUsername(userName).orElse(null);
     }
 
+    /**
+     * Checks if the provided username and password match any existing user in the database.
+     *
+     * @param userName The username of the user to be checked.
+     * @param password The password of the user to be checked.
+     * @return The UserModel object representing the authenticated user, or null if authentication fails.
+     */
     public UserModel checkUser(String userName, String password) {
         if (userName == null || password == null) {
             return null;
@@ -32,7 +55,8 @@ public class UserService {
             if (user == null) {
                 return null;
             } else {
-                if (user.getPassword().equals(password)) {
+                // Compare the provided password with the encoded password stored in the database
+                if (passwordEncoder.matches(password, user.getPassword())) {
                     return user;
                 } else {
                     return null;
