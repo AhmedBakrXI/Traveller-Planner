cedbe8a3d301dc29017ee4399435a32165f22bb8
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2024-05-14 00:30:35 +0300

    Update MyUserDetailsService.java

--- src/main/java/com/traveller/planner/service/MyUserDetailsService.java
+++ src/main/java/com/traveller/planner/service/MyUserDetailsService.java
@@ -1,3 +1,10 @@
+/**
+ * The MyUserDetailsService class implements the Spring Security UserDetailsService interface
+ * to provide custom user details retrieval for authentication purposes.
+ * It interacts with the UserRepository to fetch user details from the database.
+ */
+
+
 package com.traveller.planner.service;
 
 import com.traveller.planner.model.UserModel;
@@ -16,12 +23,21 @@ public class MyUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+     /**
+     * Loads user details by the given username.
+     *
+     * @param username The username of the user whose details are to be loaded.
+     * @return UserDetails object representing the user's details.
+     * @throws UsernameNotFoundException if the user with the specified username is not found.
+     */
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<UserModel> userModelOptional = userRepository.findByUsername(username);
         if (userModelOptional.isPresent()) {
             System.out.println(userModelOptional.get().getPassword());
             var user = userModelOptional.get();
+            // Building UserDetails object using the retrieved user's details
             return User.builder()
                     .username(user.getUsername())
                     .password(user.getPassword())
