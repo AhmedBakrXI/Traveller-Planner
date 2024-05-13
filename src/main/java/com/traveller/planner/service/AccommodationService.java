cd86b0661c1486fd4b1315556ac83ef7ac5f26d2
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2024-05-14 00:38:44 +0300

    Update AccommodationService.java

--- src/main/java/com/traveller/planner/service/AccommodationService.java
+++ src/main/java/com/traveller/planner/service/AccommodationService.java
@@ -1,3 +1,6 @@
+/**
+ * This service class provides methods to interact with accommodation data.
+ */
 package com.traveller.planner.service;
 
 import com.traveller.planner.model.AccomodationModel;
@@ -9,11 +12,17 @@ import java.util.List;
 
 @Service
 public class AccommodationService {
+
     @Autowired
     private AccommodationRepository accommodationRepository;
 
-
+    /**
+     * Retrieves all accommodations for a given destination.
+     * @param destination The destination city for which accommodations are requested.
+     * @return A list of AccomodationModel objects representing accommodations in the specified destination.
+     */
     public List<AccomodationModel> getAllAccommodations(String destination) {
+        // Retrieve accommodations by destination city name
         return accommodationRepository.findByCityName(destination);
     }
 }
