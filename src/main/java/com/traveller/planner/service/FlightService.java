e5aa4eb4a8eb4f43a31c15ec12c42542e2789436
waelmarwan7 <91396631+waelmarwan7@users.noreply.github.com>
2024-05-14 00:33:27 +0300

    Update FlightService.java

--- src/main/java/com/traveller/planner/service/FlightService.java
+++ src/main/java/com/traveller/planner/service/FlightService.java
@@ -1,3 +1,6 @@
+/**
+ * This service class provides methods to interact with flight data.
+ */
 package com.traveller.planner.service;
 
 import java.util.List;
@@ -13,15 +16,27 @@ import com.traveller.planner.repository.FlightRepository;
 
 @Service
 public class FlightService {
+
     @Autowired
     private FlightRepository flightRepository;
+    
     @Autowired
     private UserService userService;
 
+    /**
+     * Retrieves all flights for a given destination and user's city.
+     * @param destination The destination city for which flights are requested.
+     * @return A list of FlightModel objects representing flights to the specified destination from the user's city.
+     */
     public List<FlightModel> getAllFlightsByDestinationAndCity(String destination) {
+        // Retrieve currently logged-in user's username
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
+        
+        // Retrieve user model based on the username
         UserModel userModel = userService.getUser(username);
+        
+        // Retrieve flights by city name and destination
         return flightRepository.findByCityNameAndDestination(userModel.getCity(), destination);
     }
 }
