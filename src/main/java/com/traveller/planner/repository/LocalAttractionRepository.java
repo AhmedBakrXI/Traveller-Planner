1fcda8d6c77745fd50d9d37ee7599d49de473c27
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-05-15 06:13:44 +0300

    Update LocalAttractionRepository.java

--- src/main/java/com/traveller/planner/repository/LocalAttractionRepository.java
+++ src/main/java/com/traveller/planner/repository/LocalAttractionRepository.java
@@ -1,3 +1,7 @@
+/**
+ * This interface represents a repository for managing local attractions.
+ * It extends JpaRepository for CRUD operations on LocalAttractionModel entities.
+ */
 package com.traveller.planner.repository;
 
 import com.traveller.planner.model.LocalAttractionModel;
@@ -6,5 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LocalAttractionRepository extends JpaRepository<LocalAttractionModel, String> {
+
+    /**
+     * Retrieves a list of local attractions based on the specified city name.
+     *
+     * @param cityName The name of the city for which local attractions are to be retrieved.
+     * @return A list of LocalAttractionModel objects representing local attractions in the specified city.
+     */
     List<LocalAttractionModel> findByCityName(String cityName);
 }
