e76c94c1d03d8dd3bd7fdf42327cfe9649cd07a1
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-05-15 06:14:49 +0300

    Update ActivityRepository.java

--- src/main/java/com/traveller/planner/repository/ActivityRepository.java
+++ src/main/java/com/traveller/planner/repository/ActivityRepository.java
@@ -1,3 +1,7 @@
+/**
+ * This interface represents a repository for managing activities.
+ * It extends JpaRepository for CRUD operations on ActivityModel entities.
+ */
 package com.traveller.planner.repository;
 
 import com.traveller.planner.model.ActivityModel;
@@ -6,5 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ActivityRepository extends JpaRepository<ActivityModel, String> {
+
+    /**
+     * Retrieves a list of activities based on the specified city name.
+     *
+     * @param cityName The name of the city for which activities are to be retrieved.
+     * @return A list of ActivityModel objects representing activities in the specified city.
+     */
     List<ActivityModel> findByCityName(String cityName);
 }
