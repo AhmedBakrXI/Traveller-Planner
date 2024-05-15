ae7bc282b1c28a5280eb8dc562da7e898ffd9cc5
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-05-15 06:14:06 +0300

    Update FlightRepository.java

--- src/main/java/com/traveller/planner/repository/FlightRepository.java
+++ src/main/java/com/traveller/planner/repository/FlightRepository.java
@@ -1,3 +1,7 @@
+/**
+ * This interface represents a repository for managing flights.
+ * It extends JpaRepository for CRUD operations on FlightModel entities.
+ */
 package com.traveller.planner.repository;
 
 import com.traveller.planner.model.FlightModel;
@@ -6,6 +10,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FlightRepository extends JpaRepository<FlightModel, Integer> {
-    List<FlightModel> findByCityNameAndDestination(String cityName, String destination);
 
+    /**
+     * Retrieves a list of flights based on the specified departure city and destination.
+     *
+     * @param cityName    The name of the departure city.
+     * @param destination The destination city.
+     * @return A list of FlightModel objects representing flights from the specified city to the destination.
+     */
+    List<FlightModel> findByCityNameAndDestination(String cityName, String destination);
 }
