867be5fc64af0c6ed9bbda3c17ec01ff1180566a
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-05-15 06:13:21 +0300

    Update AccommodationRepository.java

--- src/main/java/com/traveller/planner/repository/AccommodationRepository.java
+++ src/main/java/com/traveller/planner/repository/AccommodationRepository.java
@@ -1,3 +1,7 @@
+/**
+ * This interface represents a repository for managing accommodations.
+ * It extends JpaRepository for CRUD operations on AccommodationModel entities.
+ */
 package com.traveller.planner.repository;
 
 import com.traveller.planner.model.AccomodationModel;
@@ -6,5 +10,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AccommodationRepository extends JpaRepository<AccomodationModel, String> {
+
+    /**
+     * Retrieves a list of accommodations based on the specified city name.
+     *
+     * @param cityName The name of the city for which accommodations are to be retrieved.
+     * @return A list of AccommodationModel objects representing accommodations in the specified city.
+     */
     List<AccomodationModel> findByCityName(String cityName);
 }
