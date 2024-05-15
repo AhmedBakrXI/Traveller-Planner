d6b3738efbb7209ad9f4bad1da3158f16c02e924
Karim-308 <105867500+Karim-308@users.noreply.github.com>
2024-05-15 06:14:27 +0300

    Update CityRepository.java

--- src/main/java/com/traveller/planner/repository/CityRepository.java
+++ src/main/java/com/traveller/planner/repository/CityRepository.java
@@ -1,8 +1,19 @@
+/**
+ * This interface represents a repository for managing cities.
+ * It extends JpaRepository for CRUD operations on CityModel entities.
+ */
 package com.traveller.planner.repository;
 
 import com.traveller.planner.model.CityModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<CityModel, String> {
+
+    /**
+     * Retrieves a city based on the specified city name.
+     *
+     * @param cityName The name of the city to retrieve.
+     * @return The CityModel object representing the city with the specified name, or null if not found.
+     */
     CityModel findByCityName(String cityName);
 }
